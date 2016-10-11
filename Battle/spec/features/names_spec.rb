c6bfff332485258f1ea0543076c20dbea93d6bf1
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-10-11 14:22:56 +0100

    things

--- Battle/spec/features/names_spec.rb
+++ Battle/spec/features/names_spec.rb
@@ -3,7 +3,7 @@ require 'app.rb'
 feature "entering names" do
   scenario "entering name via form" do
     visit '/'
-    fill_in "name", with: "Frank"
+    fill_in :name, with: "Frank"
     click_button "Submit"
 
     expect(page).to have_content("Frank")
